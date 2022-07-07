2787db85751e68662e1227737f16ff6090fb329a
unknown <pragadhesh14@gmail.com>
2022-07-07 19:28:19 +0530

    Initial configuration for anime

--- pack.ts
+++ pack.ts
@@ -1,75 +1,75 @@
 import * as coda from "@codahq/packs-sdk";
 export const pack = coda.newPack();
-const timestamp = require('time-stamp');
-const keys = require('./api-keys.json')
-const md5 = require('md5');
-pack.addNetworkDomain("gateway.marvel.com");
-const BASE_URL = "https://gateway.marvel.com"
-const IMAGE_FORMAT = "/portrait_uncanny.jpg"
-const public_key = keys.publickey
-const private_key = keys.privatekey
+pack.addNetworkDomain("gogoanime.herokuapp.com");
+const BASE_URL = "https://gogoanime.herokuapp.com/";
 
-// Schema for a Characters image.
-const CharactersSchema = coda.makeObjectSchema({
+// Schema for a Anime Name
+const AnimeDetailsSchema = coda.makeObjectSchema({
   properties: {
-    thumbnail: {
+    animeTitle: {
+      type: coda.ValueType.String,
+    },
+    type: {
+      type: coda.ValueType.String,
+    },
+    releasedDate: {
+      type: coda.ValueType.String,
+    },
+    status: {
+      type: coda.ValueType.String,
+    },
+    otherNames: {
+      type: coda.ValueType.String,
+    },
+    synopsis: {
+      type: coda.ValueType.String,
+    },
+    totalEpisodes: { type: coda.ValueType.Number },
+    genres: {
+      type: coda.ValueType.String
+    },
+    animeImg: {
       type: coda.ValueType.String,
       codaType: coda.ValueHintType.ImageReference,
     },
-    name: {
-      type: coda.ValueType.String,
-    },
-    id: { type: coda.ValueType.Number },
-    description: { type: coda.ValueType.String },
+   
   },
-  displayProperty: "thumbnail",
-  featuredProperties: ["name", "id","description"],
-  idProperty: "id",
+  displayProperty: "animeImg",
+  featuredProperties: ["animeTitle","type","releasedDate","status","otherNames","synopsis","totalEpisodes"],
+  idProperty: "animeTitle",
 });
 
+//Formula to get the details of the anime
+pack.addFormula({
+  name: "Animedetails",
+  description: "Gets the details of the anime",
+  parameters: [
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "name",
+      description: "The name of the anime",
+    }),
+  ],
+  resultType: coda.ValueType.Object,
+  schema: AnimeDetailsSchema,
 
-pack.addSyncTable({
-  name: "Characters",
-  identityName: "Characters",
-  schema: CharactersSchema,
-  connectionRequirement: coda.ConnectionRequirement.None,
-  formula: {
-    name: "getcharacters",
-    description: "Returns all the Marvel characters",
-    parameters: [],
-    execute: async function ([], context) {
-      let final_url = BASE_URL+"/v1/public/characters"
-      let ts = timestamp.utc();
-      let hash = md5(ts+private_key+public_key)
-      let url = coda.withQueryParams(final_url, {
-        ts: ts,
-        apikey: public_key,
-        hash: hash
-      });
-      let response = await context.fetcher.fetch({
-        method: "GET",
-        url: url,
-      });
-      const results = response.body.data.results;
-      const data = [];
-      for (let obj in results)
-      {
-        let id = results[obj].id
-        let name = results[obj].name
-        let description = results[obj].description
-        let thumbnail = results[obj].thumbnail.path+IMAGE_FORMAT
-        data.push({
-          thumbnail: thumbnail,
-          id: id,
-          name: name,
-          description: description
-        })
-    }
-      return {
-        result: data,
-      };
-    },
+  execute: async function ([name], context) {
+    let finalurl = BASE_URL+"anime-details/"+name
+    let response = await context.fetcher.fetch({
+      method: "GET",
+      url: finalurl,
+    });
+    return {
+      animeTitle: response.body.animeTitle,
+      type: response.body.type,
+      releasedDate: response.body.releasedDate,
+      status: response.body.status,
+      otherNames: response.body.otherNames,
+      synopsis: response.body.synopsis,
+      animeImg: response.body.animeImg,
+      totalEpisodes: response.body.totalEpisodes,
+      //genres: response.body.genres
+    };
   },
 });
-
  
