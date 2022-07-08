fa6b11ef4c5e111588efd7f718cac11591f0386f
unknown <pragadhesh14@gmail.com>
2022-07-08 15:34:19 +0530

    Added watch feature

--- pack.ts
+++ pack.ts
@@ -35,8 +35,8 @@ const AnimeDetailsSchema = coda.makeObjectSchema({
     },
    
   },
-  displayProperty: "animeTitle",
-  idProperty: "animeTitle",
+  displayProperty: "image",
+  idProperty: "image",
 });
 
 // Schema for Anime Episodes
@@ -48,13 +48,9 @@ const AnimeEpisodesSchema = coda.makeObjectSchema({
     episodeNum: {
       type: coda.ValueType.Number,
     },
-    episodeUrl: {
-      type: coda.ValueType.String,
-      codaType: coda.ValueHintType.Embed
-    },
   },
   displayProperty: "episodeNum",
-  featuredProperties: ["episodeUrl","episodeId"],
+  featuredProperties: ["episodeId"],
   idProperty: "episodeId",
 });
 
@@ -71,13 +67,9 @@ const PopularAnimeSchema = coda.makeObjectSchema({
       type: coda.ValueType.String,
       codaType: coda.ValueHintType.ImageReference
     },
-    animeUrl: {
-      type: coda.ValueType.String,
-      codaType: coda.ValueHintType.Embed
-    },
   },
   displayProperty: "animeId",
-  featuredProperties: ["animeTitle","animeImg","animeUrl"],
+  featuredProperties: ["animeTitle","animeImg"],
   idProperty: "animeId",
 });
 
@@ -97,13 +89,9 @@ const AnimeSearchSchema = coda.makeObjectSchema({
       type: coda.ValueType.String,
       codaType: coda.ValueHintType.ImageReference
     },
-    animeUrl: {
-      type: coda.ValueType.String,
-      codaType: coda.ValueHintType.Embed
-    },
   },
   displayProperty: "animeId",
-  featuredProperties: ["animeTitle","status","animeImg","animeUrl"],
+  featuredProperties: ["animeTitle","status","animeImg"],
   idProperty: "animeId",
 });
 
@@ -123,13 +111,9 @@ const AnimeMoviesSchema = coda.makeObjectSchema({
       type: coda.ValueType.String,
       codaType: coda.ValueHintType.ImageReference
     },
-    animeUrl: {
-      type: coda.ValueType.String,
-      codaType: coda.ValueHintType.Embed
-    },
   },
   displayProperty: "animeId",
-  featuredProperties: ["animeTitle","releasedDate","animeImg","animeUrl"],
+  featuredProperties: ["animeTitle","releasedDate","animeImg"],
   idProperty: "animeId",
 });
 
@@ -164,6 +148,29 @@ pack.addFormula({
       totalEpisodes: response.body.totalEpisodes,
       genres: response.body.genres
     };
+  },
+});
+
+// Formula to watch the anime
+pack.addFormula({
+  name: "WatchAnime",
+  description: "Returns the video of the anime to watch. Input should be valid episode id",
+  parameters: [
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: "name",
+      description: "The episode id",
+    }),
+  ],
+  resultType: coda.ValueType.String,
+  codaType: coda.ValueHintType.Embed,
+  execute: async function ([name], context) {
+    let finalurl = BASE_URL+"vidcdn/watch/"+name
+    let response = await context.fetcher.fetch({
+      method: "GET",
+      url: finalurl,
+    });
+    return response.body.sources[0].file
   },
 });
  
@@ -274,3 +281,34 @@ pack.addSyncTable({
   },
 });
 
+// Sync table to print the details of an anime by genre
+pack.addSyncTable({
+  name: "GetAnimeByGenre",
+  identityName: "Animeedetails",
+  schema: AnimeMoviesSchema,
+  formula: {
+    name: "GetAnimeByGenre",
+    description: "Returns the details of the animes associated with the genre",
+    parameters: [
+      coda.makeParameter({
+        type: coda.ParameterType.String,
+        name: "name",
+        description: "The genre for the anime.Supported genres - action,adventure,cars,comedy,crime,dementia,demons,drama,dub, \
+        ecchi,family,fantasy,game,gourmet,harem,historical,horror,josei,kids,magic,martial-arts,mecha,military,Mmusic,mystery, \
+        parody,police,psychological,romance,samurai,school,sci-fi,seinen,shoujo,shoujo-ai,shounen,shounen-ai,slice-of-Life,space, \
+        sports,super-power,supernatural,suspense,thriller,vampire,yaoiyuri"
+      }),
+    ],
+    execute: async function ([name], context) {
+      let finalurl = BASE_URL+"genre/"+name
+      let response = await context.fetcher.fetch({
+        method: "GET",
+        url: finalurl,
+      });
+      const data = response.body
+      return {
+        result: data
+      };
+    },
+  },
+});
