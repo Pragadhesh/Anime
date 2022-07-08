e346a856f5131fa34b9d0d108104da2230aa8f01
unknown <pragadhesh14@gmail.com>
2022-07-08 11:15:36 +0530

    Added episode details

--- pack.ts
+++ pack.ts
@@ -3,7 +3,7 @@ export const pack = coda.newPack();
 pack.addNetworkDomain("gogoanime.herokuapp.com");
 const BASE_URL = "https://gogoanime.herokuapp.com/";
 
-// Schema for a Anime Name
+// Schema for a Anime Details
 const AnimeDetailsSchema = coda.makeObjectSchema({
   properties: {
     animeTitle: {
@@ -26,20 +26,39 @@ const AnimeDetailsSchema = coda.makeObjectSchema({
     },
     totalEpisodes: { type: coda.ValueType.Number },
     genres: {
-      type: coda.ValueType.String
+      type: coda.ValueType.Array,
+      items: { type: coda.ValueType.String}
     },
-    animeImg: {
+    image: {
       type: coda.ValueType.String,
       codaType: coda.ValueHintType.ImageReference,
     },
    
   },
-  displayProperty: "animeImg",
-  featuredProperties: ["animeTitle","type","releasedDate","status","otherNames","synopsis","totalEpisodes"],
+  displayProperty: "animeTitle",
   idProperty: "animeTitle",
 });
 
-//Formula to get the details of the anime
+// Schema for Anime Episodes
+const AnimeEpisodesSchema = coda.makeObjectSchema({
+  properties: {
+    episodeId: {
+      type: coda.ValueType.String,
+    },
+    episodeNum: {
+      type: coda.ValueType.Number,
+    },
+    episodeUrl: {
+      type: coda.ValueType.String,
+      codaType: coda.ValueHintType.Embed
+    },
+  },
+  displayProperty: "episodeNum",
+  featuredProperties: ["episodeUrl","episodeId"],
+  idProperty: "episodeId",
+});
+
+// Formula to get the details of the anime
 pack.addFormula({
   name: "Animedetails",
   description: "Gets the details of the anime",
@@ -66,10 +85,41 @@ pack.addFormula({
       status: response.body.status,
       otherNames: response.body.otherNames,
       synopsis: response.body.synopsis,
-      animeImg: response.body.animeImg,
+      image: response.body.animeImg,
       totalEpisodes: response.body.totalEpisodes,
-      //genres: response.body.genres
+      genres: response.body.genres
     };
   },
 });
  
+// Sync table to print the episode details of an anime
+pack.addSyncTable({
+  name: "Episodedetails",
+  identityName: "Episodedetails",
+  schema: AnimeEpisodesSchema,
+  formula: {
+    name: "Episodedetails",
+    description: "Returns the details of all the Episodes of an anime",
+    parameters: [
+      coda.makeParameter({
+        type: coda.ParameterType.String,
+        name: "name",
+        description: "The name of the anime",
+      }),
+    ],
+    execute: async function ([name], context) {
+      let finalurl = BASE_URL+"anime-details/"+name
+      let response = await context.fetcher.fetch({
+        method: "GET",
+        url: finalurl,
+      });
+      const data = response.body.episodesList
+      data.sort((a,b) => { 
+        return a.episodeNum - b.episodeNum;
+      });
+      return {
+        result: data
+      };
+    },
+  },
+});
